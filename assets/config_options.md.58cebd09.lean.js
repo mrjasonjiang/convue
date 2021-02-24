a0c1d8752edab410281538a0f127082e4bd3726d
Ziping Li <ziping@xencapital.com>
2021-02-24 23:08:45 +0800

    deploy

---
+++ assets/config_options.md.58cebd09.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a}from"./app.d4385936.js";const t='{"title":"Options","description":"","frontmatter":{},"relativePath":"config/options.md","lastUpdated":1614179084199}',p={},o=a('',2);p.render=function(a,t,p,e,c,l){return n(),s("div",null,[o])};export default p;export{t as __pageData};
---
+++ assets/guide_locales.md.a6a30b70.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,a as l}from"./app.d4385936.js";const t='{"title":"multi-language","description":"","frontmatter":{},"headers":[{"level":2,"title":"Hot reload","slug":"hot-reload"}],"relativePath":"guide/locales.md","lastUpdated":1613802939047}',o={},r=l('',6);o.render=function(l,t,o,i,s,d){return e(),a("div",null,[r])};export default o;export{t as __pageData};
